0263e2f7c8bdf72f967ae03c2da989a8fe49450b
baanv <viktoriya_2154@mail.ru>
2024-04-13 22:59:09 +0700

    Этот файл

--- sitechina/china/apps.py
+++ sitechina/china/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ChinaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'china'
+    verbose_name = 'Информационный сайт'
