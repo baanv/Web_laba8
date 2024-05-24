8ada2138ef3b85f56907d44158f7555605b09d26
Виктория Баранова <baranova.2021@azure.nstu.ru>
2024-05-24 15:49:39 +0700

    Этот файл

---
+++ sitechina/china/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+import uuid
+from .models import Category, Translate, China
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="Файл")
+
+
+@deconstructible
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = China
+        fields = ['title', 'slug', 'content', 'annotation', 'is_published',
+                  'cat', 'tags', 'photo']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+            'annotation': forms.Textarea(attrs={'cols': 60, 'rows': 3}),
+        }
+
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                 empty_label="Категория не выбрана", label="Категории")
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов')
+        return title
